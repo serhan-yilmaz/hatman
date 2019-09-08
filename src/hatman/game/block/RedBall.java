56fa2083c563f251a7b19fa9ff5d513693dd71d9
serhan-yilmaz <serhanyilmz@gmail.com>
2019-09-07 22:41:55 -0400

    Implementation of Red Ball

---
+++ src/hatman/game/block/RedBall.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hatman.game.block;
+
+import java.awt.Color;
+import sygfx.ScaledGraphics;
+import sygfx.util.Anchor;
+
+/**
+ *
+ * @author Serhan Yilmaz <github.com/serhan-yilmaz>
+ */
+public class RedBall extends Block{
+    private double radius;
+    private Block target;
+    
+    public RedBall(double x, double y, double speed, 
+            double radius, Block target) {
+        super(x, y, speed);
+        this.radius = radius;
+        this.target = target;
+    }
+
+    @Override
+    public void draw(ScaledGraphics sg) {
+        int x = (int) this.x;
+        int y = (int) this.y;
+        int radius = (int) this.radius;
+        sg.setAnchor(Anchor.CENTER);
+        sg.setColor(Color.red);
+        sg.fillOval(x, y, 2 * radius, 2 * radius);
+    }
+
+    @Override
+    public void cycle() {
+        moveThrough(target.x, target.y, this.speed);
+    }
+    
+}
